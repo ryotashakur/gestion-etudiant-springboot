647532384d5b07079bbbb35e7bad51193e16880f
ryotashakur <mohamadoulaminedioum@esp.sn>
2025-02-21 21:44:55 +0100

    Initializing project

---
+++ gestion-etudiants/src/test/java/com/telecom3/gestion_etudiants/GestionEtudiantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom3.gestion_etudiants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEtudiantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
