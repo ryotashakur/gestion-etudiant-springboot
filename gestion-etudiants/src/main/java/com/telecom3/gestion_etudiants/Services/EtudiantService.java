0d634e1c3da647224583a84b9b7882306f027909
ryotashakur <mohamadoulaminedioum@esp.sn>
2025-02-23 04:58:26 +0100

    implementing EtudiantService finished

--- gestion-etudiants/src/main/java/com/telecom3/gestion_etudiants/Services/EtudiantService.java
+++ gestion-etudiants/src/main/java/com/telecom3/gestion_etudiants/Services/EtudiantService.java
@@ -8,23 +8,64 @@ import com.telecom3.gestion_etudiants.Repositories.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
-public class EtudiantService  {
+public class EtudiantService {
     @Autowired
     private EtudiantRepository etudiantRepository;
     @Autowired
     private ClasseService classeService;
 
     //METHODE CRUD
-    public Etudiant create(EtudiantDTO etudiantDTO){
-      Classe classe=  classeService.findById(etudiantDTO.getIdClass());
-     Etudiant etudiant= EtudiantDTO.etudiantDTOToEtudiant(etudiantDTO);
-     etudiant.setClasse(classe);
-      return etudiantRepository.save(etudiant);
+    //create
+    public Etudiant create(EtudiantDTO etudiantDTO) {
+        Classe classe = classeService.findById(etudiantDTO.getIdClass());
+        Etudiant etudiant = EtudiantDTO.etudiantDTOToEtudiant(etudiantDTO);
+        etudiant.setClasse(classe);
+        return etudiantRepository.save(etudiant);
 
     }
 
+    //read all
+    public List<Etudiant> getAll() {
+        return etudiantRepository.findAll();
+    }
 
+    //read byid
+    public Etudiant getById(long id) {
+
+        Optional<Etudiant> etudiant = etudiantRepository.findById(id);
+        if (etudiant.isPresent()) {
+            return etudiant.get();
+        }
+        throw new RuntimeException("l'etudiant n'existe pas");
+
+    }
+
+    //update
+    public Etudiant update(EtudiantDTO etudiantDTO, Long id) {
+        Etudiant etudiant = getById(id);
+        Classe classe = classeService.findById(etudiantDTO.getIdClass());
+        etudiant.setClasse(classe);
+        String nom = etudiantDTO.getNom();
+        String prenom = etudiantDTO.getPrenom();
+        LocalDate dateNaiss = etudiantDTO.getDateNaissance();
+        etudiant.setNom(nom);
+        etudiant.setPrenom(prenom);
+        etudiant.setDateNaissance(dateNaiss);
+
+        return etudiantRepository.save(etudiant);
+
+    }
+
+    //delete
+
+    public void deletebyId(long id) {
+        Etudiant etudiant = getById(id);
+        etudiantRepository.delete(etudiant);
+    }
 }
